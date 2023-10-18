89c69a999dd2d7035f2457cf1ab093ae57776d96
Zuleriqhbal <eriqhbal@gmail.com>
2023-10-18 22:49:38 +0700

    feat: finish component chat feed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {ChatEngine} from "react-chat-engine";
+import { ChatFeed } from './Components';
 import "./App.css";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <ChatEngine
+      height="100vh"
+      projectID="da0fe527-f8de-497f-a2bf-79eaf9795506"
+      userName="Eriqh"
+      userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+    />
+  );
 }
 
 export default App
