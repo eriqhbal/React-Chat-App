9fd0293b015fbeddcffd1596ba54400f01e0d4f3
Zuleriqhbal <eriqhbal@gmail.com>
2023-10-20 11:25:17 +0700

    feat: finish to communication design

--- src/Components/MessageForm.jsx
+++ src/Components/MessageForm.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
-const MessageForm = () => {
+const MessageForm = (props) => {
+  const [value, setValue] = useState('');
+  const {chatId, creds} = props
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const text = value.trim();
+
+    if(text.length > 0){
+      sendMessage(creds, chatId, {text});
+    }
+
+  }
+  const handleChangeText = (e) => {
+    setValue(e.target.value)
+    isTyping(props, chatId)
+  }
+
+  const handleUploadImage = (e) => {
+
+    sendMessage(creds, chatId, {files: e.target.files, text: ''})
+  }
   return (
-    <div>MessageForm</div>
+    <form onSubmit={handleSubmit} className='message-form'>
+      <input type="text"  value={value} onChange={handleChangeText} onSubmit={handleSubmit} className='message-input' placeholder='send a message...'/>
+      <label htmlFor="upload-button"><span className='image-button'>{<PictureOutlined className='picture-icon'/>}</span></label>
+      <input type="file" id='upload-button' multiple={false} style={{display: 'none'}} onChange={handleUploadImage}/>
+      <button type="submit" className='send-button'>{<SendOutlined className='send-icon'/>}</button>
+    </form>
   )
 }
 
--- src/Components/TheirMessage.jsx
+++ src/Components/TheirMessage.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
-const TheirMessage = () => {
+const TheirMessage = ({message, lastMessage}) => {
+  const isMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username; 
   return (
-    <div>TheirMessage</div>
-  )
+    <div className="message-row">
+      {isMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+        >
+          {<div style={{color: "black"}}>
+            {message.sender.username}
+          </div>}
+        </div>
+      )}
+      {message?.attachments?.length > 0 ? (
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachments"
+          className="message-image"
+          style={{ marginLeft: isMessageByUser ? "4px" : "48px" }}
+        />
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: "right",
+            backgroundColor: "#cabcdc",
+            marginLeft: isMessageByUser ? "4px" : "48px",
+          }}
+        >
+          {message.text}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default TheirMessage;
